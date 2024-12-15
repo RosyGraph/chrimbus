8a7d55f2bd58eee72b0230758a31630e6bcc8459
RosyGraph <chris.stevenson@live.com>
2024-12-14 22:51:53 -0700

    bah

--- main.py
+++ main.py
@@ -109,16 +109,12 @@ def pinwheel(time_limit=TIME_LIMIT):
         matrix = LEDMatrix(
             pixels=pixels,
         )
+        left_of_middle = {k for k, v in matrix.mapping.items() if v[0] < 0.5}
+        pixels[:] = [
+            red if i in left_of_middle else white for i, _ in enumerate(pixels)
+        ]
+        pixels.show()
         while True:
-            left_of_middle = [k for k, v in matrix.mapping.items() if v[0] < 0.5]
-            right_of_middle = [k for k, v in matrix.mapping.items() if v[0] > 0.5]
-            print(left_of_middle)
-            print(right_of_middle)
-            for i in left_of_middle:
-                matrix.set_pixel(i / len(left_of_middle), 0, red)
-            for i in right_of_middle:
-                matrix.set_pixel(i / len(right_of_middle), 0, white)
-            matrix.show()
             time.sleep(0.03)
             elapsed = time.time() - start
             if elapsed > time_limit * 60:
