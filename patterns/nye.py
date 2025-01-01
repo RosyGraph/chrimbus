72c7c22e363ab51cf647af2c958529e4890f8b5c
RosyGraph <chris.stevenson@live.com>
2024-12-31 23:45:06 -0700

    Fix ball drop

--- patterns/nye.py
+++ patterns/nye.py
@@ -14,13 +14,20 @@ def hue_to_rgb(hue):
     return (green, red, blue)
 
 
+def euclidean_distance(x1, y1, x2, y2):
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+
 @with_neopixel
 def nye(pixels, time_limit=TIME_LIMIT):
     matrix = LEDMatrix(pixels=pixels)
+    start = time.time()
 
     pixels.fill((0, 0, 0))
     while True:
-        now = datetime.now()
+        now = datetime(2024, 12, 31, 11, 59, 55)  # Fixed time for testing logic
+        # new year
+        # now = datetime(2025, 1, 1, 0, 0, 0)
         if now.hour != 0:
             if now.minute < 55:
                 # sunrise
@@ -30,35 +37,40 @@ def nye(pixels, time_limit=TIME_LIMIT):
                     else:
                         pixels[i] = (0, 0, 0)
                 pixels.show()
-                time.sleep(0.1)
+                time.sleep(1)
             elif now.minute > 55:
                 if now.minute < 59:
-                    pixels.fill((MAX_COLOR_VAL, MAX_COLOR_VAL, 0))
-                    time.sleep(0.1)
+                    pixels.fill((MAX_COLOR_VAL, 0, 0))
+                    pixels.show()
+                    time.sleep(0.2)
                     pixels.fill((0, 0, 0))
+                    pixels.show()
+                    time.sleep(0.2)
                 else:
                     if now.second < 50:
                         for i, (x, y) in matrix.mapping.items():
-                            pixels[i] = hue_to_rgb(y // 10)
+                            pixels[i] = (MAX_COLOR_VAL, MAX_COLOR_VAL, 0)
                             pixels.show()
                             time.sleep(0.1)
                     else:
-                        y_coord = (60 - now.second) / 10
+                        y_coord = 1 - ((60 - now.second) / 10)
+                        x_coord = 0.5
                         closest_leds = sorted(
                             [(i, (x, y)) for i, (x, y) in matrix.mapping.items()],
-                            key=lambda tup: abs(y_coord - tup[1][1]),
+                            key=lambda tup: euclidean_distance(
+                                x_coord, y_coord, *tup[1]
+                            ),
                         )[:3]
                         for i, _ in closest_leds:
                             pixels[i] = (MAX_COLOR_VAL, MAX_COLOR_VAL, 0)
                         pixels.show()
                         time.sleep(0.1)
         else:
-            # fireworks
             for i, (x, y) in matrix.mapping.items():
                 if random.random() < 0.1:
-                    pixels[i] = (MAX_COLOR_VAL, 0, 0)
+                    pixels[i] = (MAX_COLOR_VAL, MAX_COLOR_VAL, MAX_COLOR_VAL)
                 else:
-                    pixels[i] = (0, 0, 0)
+                    pixels[i] = (MAX_COLOR_VAL, MAX_COLOR_VAL, 0)
             pixels.show()
             time.sleep(0.1)
         elapsed = time.time() - start
