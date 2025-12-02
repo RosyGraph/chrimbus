4bfcfe51606e4f1a9091e6d1dbd6aa5852fcc23e
RosyGraph <chris.stevenson@live.com>
2025-12-02 14:15:42 -0700

    Fix time typo

--- challenges/c02_triangle.py
+++ challenges/c02_triangle.py
@@ -53,5 +53,5 @@ def triangle(pixels, time_limit=TIME_LIMIT):
             pixels.show()
             time.sleep(0.02)
         steps = list(reversed(steps))
-        if start - time.time() * 60 >= time_limit:
+        if (start - time.time()) * 60 >= time_limit:
             break
