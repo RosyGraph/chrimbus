6990c3ba547bd78dcd48f5b6d4ca5899e0161a76
RosyGraph <chris.stevenson@live.com>
2025-12-01 21:19:13 -0700

    Solve c02

--- challenges/c02_triangle.py
+++ challenges/c02_triangle.py
@@ -10,6 +10,7 @@ When the triangle is correctly displayed, the elves will be able to practice the
 """
 
 import math
+import numpy as np
 import time
 
 from constants import MAX_COLOR_VAL, TIME_LIMIT
@@ -17,22 +18,38 @@ from led_matrix import LEDMatrix
 from with_neopixel import with_neopixel
 
 
-def is_in_triangle(x: int, y: int):
+def is_in_triangle(x: int, y: int, f: float):
     """
-    Returns True if x, y is in the desired triangle
-    TODO: implement
+    The equation for a triangle is given by
+    max(-2y, y - x*sqrt(3), y + x*sqrt(3)) = r
+
+    Rotation about angle phi
+    x = x cos(phi) - y sin(phi)
+    y = y sin(phi) + y cos(phi)
     """
-    return False
+    r = 0.5
+    x_ = x - 0.5
+    y_ = y - 0.55
+    x_r = x_ * math.cos(f) - y_ * math.sin(f)
+    y_r = x_ * math.sin(f) + y_ * math.cos(f)
+    k = 3**0.5
+    return max(2 * y_r, -y_r + x_r * k, -y_r - x_r * k) < r
 
 
 @with_neopixel
 def triangle(pixels, time_limit=TIME_LIMIT):
-    red = (0, MAX_COLOR_VAL, 0)  # Colors are G, R, B in [0, 255]
-    # Instantiate the matrix helper
-    # Without it, we would have to deal with pixel indices directly
     matrix = LEDMatrix(pixels=pixels)
-    for led_idx, (x, y) in matrix.mapping.items():
-        if is_in_triangle(x, y):
-            pixels[led_idx] = red
-    pixels.show()
-    time.sleep(time_limit * 60)
+    start = time.time()
+    steps = list(np.linspace(0, 2 * np.pi, num=100))
+    while True:
+        for step in steps:
+            for led_idx, (x, y) in matrix.mapping.items():
+                if is_in_triangle(x, y, step):
+                    pixels[led_idx] = (MAX_COLOR_VAL, 0, 0)
+                else:
+                    pixels[led_idx] = (0, 0, 0)
+            pixels.show()
+            time.sleep(0.02)
+        steps = list(reversed(steps))
+        if start - time.time() >= time_limit:
+            break
