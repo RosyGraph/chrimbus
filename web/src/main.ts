58ab3ae0ef49692dbcf1cfe9a3d9d901dcb3cab6
Chris Stevenson <chris.stevenson@live.com>
2026-08-29 17:49:05 -0600

    feat: validate and type narrow basic animation

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,9 @@
 import "./style.css";
 import geometry from "./geometry.json";
+import basic from "./basic.json";
+import type { AnimationSchema } from "./generated/animation";
+import Ajv from "ajv";
+import schema from "./animation.schema.json";
 
 const MAX_X = 800;
 const MAX_Y = 586;
@@ -12,15 +16,21 @@ if (!canvas) {
 
 const ctx = canvas.getContext("2d");
 
-if (!ctx) {
-  throw new Error("2D canvas context unavailable");
-}
-
-function fillCirc(x, y, r, g, b) {
+function fillCirc(x: number, y: number, r: number, g: number, b: number) {
+  if (!ctx) {
+    throw new Error("2D canvas context unavailable");
+  }
   ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
   ctx.beginPath();
   ctx.arc(x, y, 5, 0, Math.PI * 2);
   ctx.fill();
+}
+
+const ajv = new Ajv();
+const validateAnimation = ajv.compile<AnimationSchema>(schema);
+const animation: unknown = basic;
+if (!validateAnimation(animation)) {
+  throw new Error("Invalid animation");
 }
 
 Object.values(geometry).forEach(([x, y]) => {
