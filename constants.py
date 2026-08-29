fa66e0890797f4e72d27a7a1548da42de9dcc89f
Chris Stevenson <46096905+RosyGraph@users.noreply.github.com>
2026-08-28 21:44:16 -0600

    chris/9 define animation format (#10)
    * chore: remove all v0 files and update README
    
    * chore: add dev shell
    
    * chore: require pnpm; ajv-cli
    
    * chore: require prettier devtool
    
    * feat: add animation schema and md file
    
    * Add simple, 2 frame example

--- constants.py
+++
@@ -1,9 +0,0 @@
-NUM_LIGHTS = 150
-MAX_COLOR_VAL = 255
-TIME_LIMIT = float("inf")
-
-COLORS = {
-    "red": (0, MAX_COLOR_VAL, 0),
-    "blue": (0, 0, MAX_COLOR_VAL),
-    "green": (MAX_COLOR_VAL, 0, 0),
-}
