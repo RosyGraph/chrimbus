d14f18d343184d8b49cb8cdfb7359b3c654a89c5
Chris Stevenson <chris.stevenson@live.com>
2026-08-29 21:50:55 -0600

    refactor: factor out set animation

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,9 @@
 import "./style.css";
 import { drawFrame } from "./renderer";
-import basic from "./examples/basic.json";
 import { validateAnimation } from "./animation";
 import { play } from "./player";
+
+const DEFAULT_EXAMPLE = "basic";
 
 const canvas = document.querySelector<HTMLCanvasElement>("#display");
 const errDisplay = document.querySelector<HTMLElement>("#error");
@@ -11,27 +12,35 @@ const examplesDiv = document.querySelector<HTMLDivElement>("#examples");
 if (!canvas || !errDisplay || !examplesDiv) {
   throw new Error("Unexpected error: missing html elements");
 }
+
 const ctx = canvas.getContext("2d");
 if (!ctx) {
   throw new Error("2D canvas context unavailable");
 }
-const animation = validateAnimation(basic);
-let currFrame = 0;
-let stop = play(animation.fps, () => {
-  drawFrame(ctx, animation.frames[currFrame]);
-  currFrame = (currFrame + 1) % animation.frames.length;
-});
+
+let stop = () => {};
 function setAnimation(ctx: CanvasRenderingContext2D, data: unknown) {
   const animation = validateAnimation(data);
   let currFrame = 0;
-
   stop();
-
   stop = play(animation.fps, () => {
     drawFrame(ctx, animation.frames[currFrame]);
     currFrame = (currFrame + 1) % animation.frames.length;
   });
 }
+
+const files = import.meta.glob("./examples/*", {
+  eager: true,
+});
+const examples = Object.fromEntries(
+  Object.entries(files).map(([path, module]) => {
+    return [
+      path.split("/").pop()!.replace(".json", ""),
+      (module as { default: unknown }).default,
+    ];
+  }),
+);
+setAnimation(ctx, examples[DEFAULT_EXAMPLE]);
 
 const upload = document.querySelector<HTMLInputElement>("#animation-upload");
 if (!upload) {
@@ -54,15 +63,7 @@ upload.addEventListener("change", async () => {
   }
 });
 
-const files = import.meta.glob("./examples/*", {
-  eager: true,
-});
-const examples = Object.entries(files).map(([path, module]) => ({
-  name: path.split("/").pop()!.replace(".json", ""),
-  data: (module as { default: unknown }).default,
-}));
-
-examples.forEach(({ name, data }) => {
+Object.entries(examples).forEach(([name, data]) => {
   const button = document.createElement("button");
   button.type = "button";
   button.className = "file-button";
